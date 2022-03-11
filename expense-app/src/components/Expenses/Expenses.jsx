6c84c38c11694ff5ae5abe04d5a06bd5aaf59fae
Kastytis S <kastytis.slubelis@gmail.com>
2022-03-11 18:31:39 +0200

    Render Chart

--- expense-app/src/components/Expenses/Expenses.jsx
+++ expense-app/src/components/Expenses/Expenses.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const expenses = props.expenses;
@@ -27,6 +27,7 @@ function Expenses(props) {
           selected={filteredYear}
           onSelectYear={yearSelectedHandler}
         />
+        <ExpensesChart expenses={filterExpenses} />
         <ExpensesList 
           expenses={filterExpenses}
         />
