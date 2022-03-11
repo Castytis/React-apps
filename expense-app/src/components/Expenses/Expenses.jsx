ace6ab63374dcedfe101c706f6bda8a20a2659f8
Kastytis S <kastytis.slubelis@gmail.com>
2022-03-11 10:44:54 +0200

    Rendering, filtering expenses

--- expense-app/src/components/Expenses/Expenses.jsx
+++ expense-app/src/components/Expenses/Expenses.jsx
@@ -15,30 +15,22 @@ function Expenses(props) {
 
   return (
     <div>
-      <ExpensesFilter 
-      selected={filteredYear}
-      onSelectYear={yearSelectedHandler} />
       <Card className="expenses">
-        <ExpenseItem
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
+        <ExpensesFilter
+          selected={filteredYear}
+          onSelectYear={yearSelectedHandler}
         />
-        <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        />
-        <ExpenseItem
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
-        />
-        <ExpenseItem
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-          date={expenses[3].date}
-        />
+        {expenses.filter((expense) => {
+           return (expense.date).getFullYear().toString() === filteredYear
+        }).map((expense) => {
+          return <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />;
+        })}
+        
       </Card>
     </div>
   );
