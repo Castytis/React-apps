e10de6c3f6c58d17c6f2a51a096a4bfc7e86b853
Kastytis S <kastytis.slubelis@gmail.com>
2022-03-13 20:23:45 +0200

    User data form

---
+++ form-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InfoForm from './components/InfoForm';
+
+function App() {
+  return (
+    <div>
+      <InfoForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ form-app/src/components/InfoForm.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Form, Container, Row, Col } from "react-bootstrap";
+
+const InfoForm = () => {
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState("");
+
+  const usernameHandler = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const ageHandler = (event) => {
+    setAge(event.target.value);
+  };
+
+  const addUserHandler = (event) => {
+    const userData = {
+      username: username,
+      age: age,
+    };
+    console.log(userData);
+
+    setUsername('')
+    setAge('')
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col xs={6}>
+            <Form onSubmit={addUserHandler}>
+              <Form.Group className="mb-3" controlId="fromBasicUsername">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  onChange={usernameHandler}
+                  value={username}
+                  type="text"
+                  placeholder="Username"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="fromBasicAge">
+                <Form.Label>Age</Form.Label>
+                <Form.Control
+                  onChange={ageHandler}
+                  value={age}
+                  type="number"
+                  placeholder="Age"
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Add User
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default InfoForm;
