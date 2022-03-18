b6c877874b6687b80a2657ec9b6633949c8f27e7
Kastytis S <kastytis.slubelis@gmail.com>
2022-03-18 19:10:00 +0200

    Working with multiple slices, splitting slices

--- redux-counter-app/src/App.js
+++ redux-counter-app/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import Counter from './components/Counter';
-
+import React from "react";
+import Auth from "./components/Auth";
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import {useSelector } from "react-redux";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
   return (
-    <Counter />
+    <React.Fragment>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+      <Counter />
+    </React.Fragment>
   );
 }
 
