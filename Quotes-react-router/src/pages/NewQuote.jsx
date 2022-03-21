5e3ebb555e9bf4e0a3a6b27dc166b22d8d978623
Kastytis S <kastytis.slubelis@gmail.com>
2022-03-21 11:14:16 +0200

    Sending, getting quote data via Http

--- Quotes-react-router/src/pages/NewQuote.jsx
+++ Quotes-react-router/src/pages/NewQuote.jsx
@@ -1,16 +1,33 @@
-import QuoteForm from '../components/quotes/QuoteForm'
-import { useHistory } from 'react-router-dom'
+import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from "react-router-dom";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
+import { useEffect } from "react";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
 const NewQuote = () => {
-    const history = useHistory()
+  const { sendRequest, status } = useHttp(addQuote);
+  const history = useHistory();
 
-    const addQuoteHandler = (quoteData) => {
-        console.log(quoteData)
-        
-        history.push('/quotes')
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
     }
+  }, [status, history]);
 
-    return <QuoteForm onAddQuote={addQuoteHandler} />
-}
+  if (status === 'pending') {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    )
+  }
 
-export default NewQuote
+  const addQuoteHandler = (quoteData) => {
+    sendRequest(quoteData);
+  };
+
+  return <QuoteForm isLoading={ status === 'pending'} onAddQuote={addQuoteHandler} />;
+};
+
+export default NewQuote;
--- Quotes-react-router/src/pages/QuoteDetail.jsx
+++ Quotes-react-router/src/pages/QuoteDetail.jsx
@@ -1,31 +1,50 @@
+import { useEffect } from "react";
 import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
-
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Mike", text: "My book" },
-  { id: "q2", author: "Mikey", text: "My other book" },
-];
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 
 const QuoteDetail = () => {
-  const match = useRouteMatch()
-  const  quoteDetail = useParams();
+  const match = useRouteMatch();
+  const quoteDetail = useParams();
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === quoteDetail.quoteId);
+  const { quoteId } = quoteDetail;
 
-  if (!quote) {
-    return <p>No quote found</p>;
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
+    return <p>No quote Found</p>
   }
 
   return (
     <div>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={match.path} exact>
         <div className="centered">
-          <Link
-            className="btn--flat"
-            to={`${match.url}/comments`}
-          >
+          <Link className="btn--flat" to={`${match.url}/comments`}>
             Load comments
           </Link>
         </div>
